e64c1218926bdb4192c2a64a6c5e42bfa3da611c
Juan Diego Mejía Maestre <mejiamaestrejuandiego@gmail.com>
2023-02-25 10:49:56 -0500

    agregar home

---
+++ src/app/website/website.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { WebsiteRoutingModule } from './website-routing.module';
+import { HomeComponent } from './pages/home/home.component';
+
+
+@NgModule({
+  declarations: [
+    HomeComponent
+  ],
+  imports: [
+    CommonModule,
+    WebsiteRoutingModule
+  ]
+})
+export class WebsiteModule { }
