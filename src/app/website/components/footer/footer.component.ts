3c0d69399635e96c6b8e5279c092cffe9c70ad1f
JuanDiegoWS <mejiamaestrejuandiego@gmail.com>
2023-10-14 22:12:39 -0500

    creacion de footer

--- src/app/website/components/footer/footer.component.ts
+++ src/app/website/components/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SettingService } from '../../services/setting.service';
 
 @Component({
   selector: 'app-footer',
@@ -6,5 +7,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent {
+  year = new Date().getFullYear();
+  data = {
+    "home": "Inicio ",
+    "about_me": "Sobre mi",
+    "services": "Servicios",
+    "skills": "Habilidades",
+    "projects": "Proyectos",
+    "blog": "Blog",
+    "contact_me": "Contáctame"
+  };
+  constructor(private setting: SettingService) {
+
+  }
+  ngOnInit(): void {
+    this.setting.lang$.subscribe(data => {
+      this.data = this.setting.data.navigation;
+    })
+  }
+
+  toGo(name: string) {
+    window.history.pushState({}, '', name);
+    setTimeout(() => {
+      const element: HTMLElement = document.getElementById(name) as HTMLElement;
+      const rect = element.getBoundingClientRect();
+      const topOffset = window.pageYOffset + rect.top - 5 * parseFloat(getComputedStyle(document.documentElement).fontSize);
+      window.scrollTo({ top: topOffset, behavior: 'smooth' });
+    }, 200);
+  }
 
 }
